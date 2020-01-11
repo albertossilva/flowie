9574e2fd5f9ffdfb3802797dd4d0ea32a623c04b
Alberto Silva <albertossilva@gmail.com>
2020-01-11 17:09:05 +0100

    One path flows

---
+++ src/__tests__/flowie.test.ts
@@ -0,0 +1,61 @@
+import { assert } from 'chai'
+import { mock, assert as sinonAssert, SinonStub } from 'sinon'
+
+import flowie from '../flowie'
+
+describe('flowie', function () {
+  describe('@executeFlow', function () {
+    const parameter = 'ARGUMENT'
+    const finalResult = 'FINAL RESULT'
+
+    it('executes and returns the result of synchronous function', async function () {
+      const commonFunction = createSimpleFunctionMock(parameter, finalResult)
+
+      const { result } = await flowie(commonFunction).executeFlow(parameter)
+
+      assert.equal(result, finalResult)
+      sinonAssert.calledOnce(commonFunction as SinonStub)
+    })
+
+    it('executes and returns the result of the second synchronous function, working as a chain', async function () {
+      const firstFunctionReturn = 'RESULT'
+      const firstFunction = createSimpleFunctionMock(parameter, firstFunctionReturn)
+
+      const secondFunction = createSimpleFunctionMock(firstFunctionReturn, finalResult)
+
+      const { result } = await flowie(firstFunction)
+        .pipeTo(secondFunction)
+        .executeFlow(parameter)
+
+      assert.equal(result, finalResult)
+    })
+
+    it('pipes multiple synchronous function', async function () {
+      const firstFunctionReturn = 'RESULT'
+      const firstFunction = createSimpleFunctionMock(parameter, firstFunctionReturn)
+      const middleWareFunction = createSimpleFunctionMock(firstFunctionReturn, firstFunctionReturn, 6)
+
+      const lastFunction = createSimpleFunctionMock(firstFunctionReturn, finalResult)
+
+      const { result } = await flowie(firstFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(middleWareFunction)
+        .pipeTo(lastFunction)
+        .executeFlow(parameter)
+
+      assert.equal(result, finalResult);
+      (middleWareFunction as any).verify()
+    })
+  })
+})
+
+function createSimpleFunctionMock (parameter: string, result: string, timesToBeExecuted = 1): (argument: string) => string {
+  return mock()
+    .withArgs(parameter)
+    .exactly(timesToBeExecuted)
+    .returns(result)
+}
